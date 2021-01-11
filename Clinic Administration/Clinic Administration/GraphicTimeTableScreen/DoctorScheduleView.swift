965eebf4f79c4a10dbcd133d144bdef4306f33c7
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2021-01-11 12:44:22 +0300

    added algorithm calculating intersected schedules

--- Clinic Administration/Clinic Administration/GraphicTimeTableScreen/DoctorScheduleView.swift
+++ Clinic Administration/Clinic Administration/GraphicTimeTableScreen/DoctorScheduleView.swift
@@ -10,16 +10,20 @@ import UIKit
 final class DoctorScheduleView: UIView {
     
     let transformArea = UIView()
-    let topResizingPoint = UIView()
-    let bottomResizingPoint = UIView()
+    private let topResizingPoint = UIView()
+    private let bottomResizingPoint = UIView()
     
     private let nameLabel = UILabel()
     
-    private var originalLocation = CGPoint()
-    private var originalHeight = CGFloat()
+    private let minuteHeight: CGFloat
     
-    private var quarterHourHeight: CGFloat {
-        GraphicTableView.Size.minuteHeight * 15
+    private var originalLocation = CGPoint.zero
+    private var originalHeight = CGFloat.zero
+    private var minutesInterval = CGFloat.zero
+    
+    /// Данное свойство является true, если расписание пересекающееся.
+    private var isIntersected: Bool! {
+        return intersectionDetection?(schedule)
     }
     
     private enum Mode {
@@ -36,31 +40,45 @@ final class DoctorScheduleView: UIView {
     private var mode: Mode = .viewing {
         didSet {
             for view in [topResizingPoint, transformArea, bottomResizingPoint] {
-                view.isUserInteractionEnabled = mode == .editing ? true : false
+                view.isUserInteractionEnabled = mode == .editing
             }
         }
     }
     
+    private(set) var schedule: DoctorSchedule
+    
+    /// Замыкание, которое выясняет является ли расписание пересекающимся.
+    private var intersectionDetection: ((DoctorSchedule) -> Bool)?
+    
+    /// Замыкание, которое выводит вью доктора на передний план.
+    ///
+    /// Используется, когда экземпляр `DoctorScheduleView` переводится в режим редактирования или когда является пересекающимся.
+    private var moveToFrontAction: ((DoctorScheduleView) -> Void)?
+    
     override func draw(_ rect: CGRect) {
         layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: Design.Shape.largeCornerRadius).cgPath
-        layer.shadowOffset = CGSize(width: 0, height: 3)
-        layer.shadowOpacity = mode == .viewing ? 0.2 : 0
         layer.shadowColor = Design.Color.brown.cgColor
-        layer.shadowRadius = 6
     }
     
-    init(_ schedule: DoctorSchedule) {
+    /// Инициализирует `DoctorScheduleView`.
+    /// - Parameters:
+    ///   - schedule: Расписание доктора.
+    ///   - minuteHeight: Высота минуты на экране.
+    ///   - intersectionDetection: Метод, который выясняет является ли расписание пересекающимся.
+    ///   - moveToFrontAction: Метод, которое выводит вью доктора на передний план.
+    init(_ schedule: DoctorSchedule, minuteHeight: CGFloat, intersectionDetection: @escaping (DoctorSchedule) -> Bool, moveToFrontAction: @escaping (DoctorScheduleView) -> Void) {
+        self.schedule = schedule
+        self.minuteHeight = minuteHeight
+        self.intersectionDetection = intersectionDetection
+        self.moveToFrontAction = moveToFrontAction
         super.init(frame: .zero)
         
-        layer.backgroundColor = Design.Color.lightGray.withAlphaComponent(0.75).cgColor
         layer.cornerRadius = Design.Shape.largeCornerRadius
-        layer.borderColor = Design.Color.darkGray.cgColor
-        layer.borderWidth = 1
-        
+        setView()
+
         nameLabel.numberOfLines = 0
         nameLabel.text = "\(schedule.secondName)\n\(schedule.firstName)\n\(schedule.patronymicName)"
         nameLabel.textAlignment = .center
-        nameLabel.textColor = Design.Color.chocolate
         nameLabel.font = Design.Font.medium(13)
         nameLabel.sizeToFit()
         addSubview(nameLabel)
@@ -114,50 +132,101 @@ final class DoctorScheduleView: UIView {
         bottomResizingPoint.isUserInteractionEnabled = false
     }
     
+    /// Данный метод используется для проверки текущего статуса и установки нужного состояния объекта `DoctorScheduleView`
+    func setView() {
+        switch mode {
+        case .editing:
+            layer.backgroundColor = Design.Color.lightGray.withAlphaComponent(0.35).cgColor
+            layer.borderColor = Design.Color.darkGray.withAlphaComponent(0.6).cgColor
+            layer.borderWidth = 1
+            layer.shadowOpacity = 0
+            nameLabel.textColor = Design.Color.chocolate.withAlphaComponent(0.6)
+            transform = .init(scaleX: 1.1, y: 1)
+            // Если в режиме редактирования то через замыкание переместим вью на передний план.
+            moveToFrontAction?(self)
+        case .viewing:
+            nameLabel.textColor = Design.Color.chocolate
+            transform = .identity
+            if isIntersected {
+                layer.backgroundColor = Design.Color.lightGray.withAlphaComponent(0.2).cgColor
+                layer.borderColor = Design.Color.red.cgColor
+                layer.borderWidth = 2
+                layer.shadowOffset = CGSize(width: 0, height: 6)
+                layer.shadowRadius = 12
+                layer.shadowOpacity = 0.15
+                // Если расписаие пересекает другие, то через замыкание переместим вью на передний план.
+                moveToFrontAction?(self)
+            } else {
+                layer.backgroundColor = Design.Color.lightGray.withAlphaComponent(0.75).cgColor
+                layer.borderColor = Design.Color.darkGray.cgColor
+                layer.borderWidth = 1
+                layer.shadowOffset = CGSize(width: 0, height: 3)
+                layer.shadowRadius = 6
+                layer.shadowOpacity = 0.2
+            }
+        }
+    }
+    
+    /// Опции редактирования расписания доктора.
+    enum ScheduleEditOption {
+        case startingTime
+        case endingTime
+        // Вероятно тут еще и кабинет появится со временем.
+    }
+    
+    
+    /// Данный метод используется для редактирования расписания доктора.
+    /// - Parameters:
+    ///   - options: Опции редактирования расписания доктора.
+    ///   - timeInterval: Временной интервал в секундах для изменения расписания доктоа.
+    func editSchedule(options: Set<ScheduleEditOption>, by timeInterval: TimeInterval) {
+        if options.contains(.startingTime) {
+            var startingDate = Calendar.current.date(from: schedule.startingTime)!
+            startingDate += timeInterval
+            schedule.startingTime = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: startingDate)
+        }
+        if options.contains(.endingTime) {
+            var endingDate = Calendar.current.date(from: schedule.endingTime)!
+            endingDate += timeInterval
+            schedule.endingTime = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: endingDate)
+        }
+    }
+    
     @objc func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer) {
         let generator = UINotificationFeedbackGenerator()
         gesture.minimumPressDuration = 0.7
         switch gesture.state {
         case .began:
-            UIView.animate(withDuration: gesture.minimumPressDuration,
+            mode = mode.change
+            UIView.animate(withDuration: 0.7,
                             delay: 0,
                             usingSpringWithDamping: 0.3,
                             initialSpringVelocity: 1,
                             options: .curveEaseOut) {
-                if self.mode == .editing {
-                    self.transform = .identity
-                    self.layer.backgroundColor = Design.Color.lightGray.withAlphaComponent(0.75).cgColor
-                    self.layer.borderColor = Design.Color.darkGray.cgColor
-                    self.layer.shadowOpacity = 0.2
-                    self.nameLabel.textColor = Design.Color.chocolate
-                } else {
-                    self.layer.backgroundColor = Design.Color.lightGray.withAlphaComponent(0.35).cgColor
-                    self.layer.borderColor = Design.Color.darkGray.withAlphaComponent(0.6).cgColor
-                    self.layer.shadowOpacity = 0
-                    self.nameLabel.textColor = Design.Color.chocolate.withAlphaComponent(0.6)
-                    self.transform = CGAffineTransform(scaleX: 1.1, y: 1)
-                }
+                self.setView()
                 generator.notificationOccurred(.success)
             }
-        case .ended:
-            mode = mode.change
         default: break
         }
     }
     
     @objc func handleBottomPanGesture(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: self)
-        let tY = translation.y - translation.y.truncatingRemainder(dividingBy: quarterHourHeight / 3)
+        let tY = translation.y - translation.y.truncatingRemainder(dividingBy: 5 * minuteHeight)
         guard let cabinetView = superview else { return }
         switch gesture.state {
         case .began:
             originalLocation.y = frame.origin.y
             originalHeight = frame.height
         case .changed:
-            let minTY = quarterHourHeight - originalHeight
+            let minTY = 15 * minuteHeight - originalHeight
             let maxTY = cabinetView.frame.height - originalLocation.y - originalHeight - 1
             frame.size.height = originalHeight + max(min(tY, maxTY), minTY)
+            // Переводим translation в минуты.
+            minutesInterval = max(min(tY, maxTY), minTY) / minuteHeight
         case .ended:
+            // Меняем расписание доктора.
+            editSchedule(options: [.endingTime], by: TimeInterval(minutesInterval * 60))
             setNeedsDisplay()
         default: break
         }
@@ -165,17 +234,21 @@ final class DoctorScheduleView: UIView {
     
     @objc func handleTopPanGesture(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: self)
-        let tY = translation.y - translation.y.truncatingRemainder(dividingBy: quarterHourHeight / 3)
+        let tY = translation.y - translation.y.truncatingRemainder(dividingBy: minuteHeight * 5)
         switch gesture.state {
         case .began:
             originalLocation.y = frame.origin.y
             originalHeight = frame.height
         case .changed:
             let minTY = -originalLocation.y
-            let maxTY = originalHeight - quarterHourHeight
+            let maxTY = originalHeight - 15 * minuteHeight
             frame.size.height = originalHeight - min(max(tY, minTY), maxTY)
             frame.origin.y = originalLocation.y + max(min(tY, maxTY), minTY)
+            // Переводим translation в минуты.
+            minutesInterval = max(min(tY, maxTY), minTY) / minuteHeight
         case .ended:
+            // Меняем расписание доктора.
+            editSchedule(options: [.startingTime], by: TimeInterval(minutesInterval * 60))
             setNeedsDisplay()
         default: break
         }
