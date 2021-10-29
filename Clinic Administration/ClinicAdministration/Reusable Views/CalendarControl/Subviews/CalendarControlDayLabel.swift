a7f552033206ce432eda29c227ceeee1664f133c
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2021-10-29 16:48:07 +0300

    Created CalendarControl

---
+++ Clinic Administration/ClinicAdministration/Reusable Views/CalendarControl/Subviews/CalendarControlDayLabel.swift
@@ -0,0 +1,39 @@
+//
+//  CalendarControlDayLabel.swift
+//  ClinicAdministration
+//
+//  Created by Nikolai Faustov on 22.10.2021.
+//
+
+import UIKit
+import HorizonCalendar
+
+struct CalendarControlDayLabel: CalendarItemViewRepresentable {
+    struct InvariantViewProperties: Hashable {
+        let font: UIFont
+        var textColor: UIColor
+        var backgroundColor: UIColor
+    }
+
+    struct ViewModel: Equatable {
+        let day: Day
+    }
+
+    static func makeView(withInvariantViewProperties invariantViewProperties: InvariantViewProperties) -> UILabel {
+        let label = UILabel()
+
+        label.font = invariantViewProperties.font
+        label.textColor = invariantViewProperties.textColor
+        label.backgroundColor = invariantViewProperties.backgroundColor
+
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.layer.cornerRadius = Design.CornerRadius.small
+
+        return label
+    }
+
+    static func setViewModel(_ viewModel: ViewModel, on view: UILabel) {
+        view.text = "\(viewModel.day.day)"
+    }
+}
