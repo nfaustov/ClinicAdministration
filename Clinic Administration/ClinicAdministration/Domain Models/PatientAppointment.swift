46a4a7236a461979d7d60a609b9b45ed202f0452
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2022-12-27 18:17:59 +0300

    Added id property to PatientAppointment model

--- Clinic Administration/ClinicAdministration/Domain Models/PatientAppointment.swift
+++ Clinic Administration/ClinicAdministration/Domain Models/PatientAppointment.swift
@@ -8,17 +8,20 @@
 import Foundation
 
 struct PatientAppointment: Codable, Hashable {
+    let id: UUID?
     var scheduledTime: Date
     var duration: TimeInterval
     var patient: Patient?
 
-    init(scheduledTime: Date, duration: TimeInterval, patient: Patient?) {
+    init(id: UUID? = UUID(), scheduledTime: Date, duration: TimeInterval, patient: Patient?) {
+        self.id = id
         self.scheduledTime = scheduledTime
         self.duration = duration
         self.patient = patient
     }
 
     init?(entity: PatientAppointmentEntity) {
+        self.id = UUID()
         scheduledTime = entity.scheduledTime ?? Date()
         duration = entity.duration
 
