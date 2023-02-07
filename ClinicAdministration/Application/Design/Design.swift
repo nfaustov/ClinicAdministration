036bbb196d2470a47b4a838636ac38bcd55b783f
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-02-08 00:18:26 +0300

    Created new Design.Label API

--- ClinicAdministration/Application/Design/Design.swift
+++ ClinicAdministration/Application/Design/Design.swift
@@ -59,6 +59,21 @@ enum Design {
 
     /// An object that generates labels
     enum Label {
+        /// An object that stores values for font size properties
+        enum Size: CGFloat {
+            /// Font size value 11
+            case labelSmall = 11
+            /// Font size value 14
+            case titleSmall = 14
+            /// Font size value 16
+            case titleMedium = 16
+            /// Font size value 18
+            case titleLarge = 18
+            /// Font size value 22
+            case headlineSmall = 22
+            /// Font size value 24
+            case headlineMedium = 24
+        }
         /// Creates date label from specified date
         /// - Parameters:
         ///   - date: date
@@ -117,6 +132,163 @@ enum Design {
             label.textColor = Design.Color.lightGray
 
             return label
+        }
+        
+        // MARK: - By font weight
+
+        /// Creates label with thin font wheight
+        /// - Parameters:
+        ///   - size: font size
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified size, text color and title.
+        static func thin(ofSize size: Label.Size, color: UIColor, withText text: String = "") -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = Design.Font.robotoFont(ofSize: size.rawValue, weight: .thin)
+            label.textColor = color
+
+            return label
+        }
+
+        /// Creates label with light font wheight
+        /// - Parameters:
+        ///   - size: font size
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified size, text color and title.
+        static func light(ofSize size: Label.Size, color: UIColor, withText text: String = "") -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = Design.Font.robotoFont(ofSize: size.rawValue, weight: .light)
+            label.textColor = color
+
+            return label
+        }
+
+        /// Creates label with regular font wheight
+        /// - Parameters:
+        ///   - size: font size
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified size, text color and title.
+        static func regular(ofSize size: Label.Size, color: UIColor, withText text: String = "") -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = Design.Font.robotoFont(ofSize: size.rawValue, weight: .regular)
+            label.textColor = color
+
+            return label
+        }
+
+        /// Creates label with medium font wheight
+        /// - Parameters:
+        ///   - size: font size
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified size, text color and title.
+        static func medium(ofSize size: Label.Size, color: UIColor, withText text: String = "") -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = Design.Font.robotoFont(ofSize: size.rawValue, weight: .medium)
+            label.textColor = color
+
+            return label
+        }
+
+        /// Creates label with bold font wheight
+        /// - Parameters:
+        ///   - size: font size
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified size, text color and title.
+        static func bold(ofSize size: Label.Size, color: UIColor, withText text: String = "") -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = Design.Font.robotoFont(ofSize: size.rawValue, weight: .bold)
+            label.textColor = color
+
+            return label
+        }
+
+        /// Creates label with black font wheight
+        /// - Parameters:
+        ///   - size: font size
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified size, text color and title.
+        static func black(ofSize size: Label.Size, color: UIColor, withText text: String = "") -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = Design.Font.robotoFont(ofSize: size.rawValue, weight: .black)
+            label.textColor = color
+
+            return label
+        }
+
+        // MARK: - By style
+        
+        /// Creates label with medium font weight of size 11
+        /// - Parameters:
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified text color and title.
+        static func labelSmall(color: UIColor, withText text: String = "") -> UILabel {
+            medium(ofSize: .labelSmall, color: color, withText: text)
+        }
+
+        /// Creates label with regular font weight of size 14
+        /// - Parameters:
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified text color and title.
+        static func labelLarge(color: UIColor, withText text: String = "") -> UILabel {
+            regular(ofSize: .titleSmall, color: color, withText: text)
+        }
+
+        /// Creates label with medium font weight of size 14
+        /// - Parameters:
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified text color and title.
+        static func titleSmall(color: UIColor, withText text: String = "") -> UILabel {
+            medium(ofSize: .titleSmall, color: color, withText: text)
+        }
+
+        /// Creates label with regular font weight of size 16
+        /// - Parameters:
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified text color and title.
+        static func titleMedium(color: UIColor, withText text: String = "") -> UILabel {
+            regular(ofSize: .titleMedium, color: color, withText: text)
+        }
+
+        /// Creates label with regular font weight of size 18
+        /// - Parameters:
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified text color and title.
+        static func titleLarge(color: UIColor, withText text: String = "") -> UILabel {
+            regular(ofSize: .titleLarge, color: color, withText: text)
+        }
+
+        /// Creates label with medium font weight of size 22
+        /// - Parameters:
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified text color and title.
+        static func headlineSmall(color: UIColor, withText text: String = "") -> UILabel {
+            medium(ofSize: .headlineSmall, color: color, withText: text)
+        }
+
+        /// Creates label with medium font weight of size 24
+        /// - Parameters:
+        ///   - color: text color
+        ///   - text: text
+        /// - Returns: `UILabel` with roboto font, specified text color and title.
+        static func headlineMedium(color: UIColor, withText text: String = "") -> UILabel {
+            medium(ofSize: .headlineMedium, color: color, withText: text)
         }
     }
 }
